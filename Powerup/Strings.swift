81d6ddd9e14fb07dad0be1dd418b47abd429570b
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-02-28 17:58:29 +0530

    moving strins of VocabMatchingGameScene.swift into Strings.swift file

---
+++ Powerup/Strings.swift
@@ -0,0 +1,12 @@
+//
+//  Strings.swift
+//  Powerup
+//
+//  Created by SIMRANPREET KAUR on 28/02/18.
+//  Copyright © 2018 Systers. All rights reserved.
+//
+
+import Foundation
+// Strings
+let endSceneTitleLabelText = "Game Over"
+let scoreLabelPrefix = "Score: "

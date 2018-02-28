f4ac27dffb1161bb678b775db90d99733e893689
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-02-28 19:47:07 +0530

    Removing unneccesary comments in String.swift

--- Powerup/Strings.swift
+++ Powerup/Strings.swift
@@ -1,11 +1,3 @@
-//
-//  Strings.swift
-//  Powerup
-//
-//  Created by SIMRANPREET KAUR on 28/02/18.
-//  Copyright © 2018 Systers. All rights reserved.
-//
-
 import Foundation
 // Strings
 let endSceneTitleLabelText = "Game Over"
